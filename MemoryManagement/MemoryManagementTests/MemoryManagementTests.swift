f14aae7e51f0d053cf9dcb6c55a2ecaed95ae984
Luakkk <a_smanova@kbtu.kz>
2025-02-23 22:07:36 +0500

    feat: add MemoryManagement folder

---
+++ MemoryManagement/MemoryManagementTests/MemoryManagementTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryManagementTests.swift
+//  MemoryManagementTests
+//
+//  Created by Alua Smanova on 23.02.2025.
+//
+
+import Testing
+@testable import MemoryManagement
+
+struct MemoryManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
